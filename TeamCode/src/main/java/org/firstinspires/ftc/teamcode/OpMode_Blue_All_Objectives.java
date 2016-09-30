f68bbae49c54526d4c86f7f20b01ac8944e2cbc1
olivervazquez0110 <olivervasquez37@yahoo.com>
2016-09-29 20:44:11 -0500

    Update OpMode_Blue_All_Objectives.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpMode_Blue_All_Objectives.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpMode_Blue_All_Objectives.java
@@ -101,8 +101,8 @@ public class PushbotAutoDriveByEncoder_Linear extends LinearOpMode {
 
         // Send telemetry message to indicate successful Encoder reset
         telemetry.addData("Path0",  "Starting at %7d :%7d",
-                          robot.leftMotor.getCurrentPosition(),
-                          robot.rightMotor.getCurrentPosition());
+                          robot.leftMotor.getCurrentPosition(), // Gets current encoder value for leftMotor
+                          robot.rightMotor.getCurrentPosition()); // Gets current encoder value for rightMotor
         telemetry.update();
 
         // Wait for the game to start (driver presses PLAY)
@@ -110,7 +110,7 @@ public class PushbotAutoDriveByEncoder_Linear extends LinearOpMode {
 
         // Step through each leg of the path,
         // Note: Reverse movement is obtained by setting a negative distance (not speed)
-        encoderDrive(DRIVE_SPEED,  48,  48, 5.0);  // S1: Forward 47 Inches with 5 Sec timeout
+        encoderDrive(DRIVE_SPEED,  48,  48, 5.0);  // S1: Forward 48 Inches with 5 Sec timeout
         encoderDrive(TURN_SPEED,   12, -12, 4.0);  // S2: Turn Right 12 Inches with 4 Sec timeout
         encoderDrive(DRIVE_SPEED, -24, -24, 4.0);  // S3: Reverse 24 Inches with 4 Sec timeout
 
