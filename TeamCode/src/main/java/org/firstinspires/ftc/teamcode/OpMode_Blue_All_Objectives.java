8f92e3fad4ead21532142a8284f1ea24f54dc659
olivervazquez0110 <olivervasquez37@gmail.com>
2016-10-30 11:52:58 -0500

    Testing Comment #2

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpMode_Blue_All_Objectives.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpMode_Blue_All_Objectives.java
@@ -41,7 +41,8 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 // (c) 2016 - FTC Team 11242 - Error 404 - Ferris High School - Ferris, TX
 
 /**
-TEST COMMENT
+
+ Test Comment #2
 
  * This file illustrates the concept of driving a path based on encoder counts.
  * It uses the common Pushbot hardware class to define the drive on the robot.
